dd0929ea64eb01f353c3a964b9338c412ec551b0
Lukas Eichler <lukas.eichler@yahoo.com>
2023-04-24 21:50:05 +1000

    added login and logout to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to 'sessions#new'
+  post 'login', to 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
